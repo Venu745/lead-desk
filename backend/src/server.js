e98f8dc85aa4eeaa8b396b9d1988e2732b4a9fa8
Venu <chandrasekharbarri77@gmail.com>
2026-07-25 16:50:23 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,27 +2,39 @@ import express from 'express'
 import noteRoutes from './routes/notesRoutes.js'
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
-// import rateLimit from './config/upstash.js';
-// import rateLimiter from './middleware/rateLimiter.js';
 import cors from 'cors'
 import authRoutes from './routes/authRoutes.js'
+import path from 'path'
 
 dotenv.config();
 console.log(process.env.MONGO_URI);
 
 const app = express();
 const PORT = process.env.PORT || 3000
+const __dirname = path.resolve()
 
 //  middleware
 
 app.use(express.json())
 // app.use(rateLimiter);
-app.use(cors(
+if(process.env.NODE_ENV !== 'production'){
+    app.use(cors(
     {origin : 'http://localhost:5174'}
 ))
+}
+
 
 app.use('/api/notes', noteRoutes);
 app.use('/api/auth',authRoutes)
+
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+app.get('/*splat',(req, res) => {
+    res.sendFile(path.join(__dirname,'../frontend','dist','index.html'))
+})
+
+}
 
 connectDB().then(()=>{
       app.listen(PORT, () => {
