c519281af2b3882a551497b89b6fc8173635c57b
Venu <chandrasekharbarri77@gmail.com>
2026-07-25 17:45:12 +0530

    prepare frontend deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,12 @@
+import axios from "axios";
 
-import axios from 'axios'
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000/api"
+    : "https://lead-desk-1.onrender.com/api";
 
-const BASE_URL = import.meta.env.MODE === 'development' ? 'http://localhost:3000/api' : '/api';
 const api = axios.create({
-    baseURL: 'https://lead-desk-1.onrender.com'
-})
+  baseURL: BASE_URL,
+});
 
 export default api;
